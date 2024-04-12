0954f7749e48121fa37b399f5caa76f69faeee19
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-04-12 22:02:33 +0330

    site template added
    + .html files added to templates
    + static files of template added to statics
    + static urls of index.html convert to dynamic with template tag'

--- website/views.py
+++ website/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def index_view(request):
-    return render(request,'index.html')
+    return render(request,'mysite\\index.html')
 def about_view(request):
-    return render(request,'about.html')
+    return render(request,'mysite\\about.html')
 def contact_view(request):
-    return render(request,'contact.html')
+    return render(request,'mysite\\contact.html')
