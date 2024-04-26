80fbe7ae80ad02c6a50f7c6c86d7cb2e54930733
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-04-26 23:21:24 +0330

    contact model added to project
    contact model and admin panel defined.

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from website.models import Contact
 # Register your models here.
+
+class contactAdmin(admin.Model):
+    date_hierarchy = 'created_date'    
+    list_display = ['name', 'email', 'created_date']
+    list_filter = ('email',)
+    search_field = ['name', 'message']
+
+admin.site.register(Contact,contactAdmin)
