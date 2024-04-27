aa300a9a339dfb8b6b8edd24e21bab3bb823567c
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-04-27 18:03:17 +0330

    data retrive testing
    creating simple page and url in views.py to test reading data from database of project.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from blog.models import Post
+#from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def blog_view(request):
@@ -8,3 +9,8 @@ def blog_view(request):
 
 def blog_single(request):
     return render(request, 'blog/blog-single.html')
+
+def test(request):
+    post = Post.objects.all()
+    content = {'posts':post}
+    return render(request, 'blog/test.html',content)
