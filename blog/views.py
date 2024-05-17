ba5e5f69835ff80817de88ac547ed9fa35798226
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-05-17 21:56:20 +0330

    dynamic blog post and views added to project.

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.utils import timezone
 #from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def blog_view(request):
     #'blog/blog-home.html' or 'blog\\blog-home.html'
-    post = Post.objects.filter(status = 1)
+    post = Post.objects.filter(published_date__lte=timezone.now())#if postTime < now, post published.
     content = {'posts':post}
     return render(request, 'blog/blog-home.html',content)
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request,pid):    
+    post = get_object_or_404(Post,id=pid)
+    post.counted_views = post.counted_views+1
+    post.save()
+    content = {'post':post}
+    return render(request, 'blog/blog-single.html',content)
 
 def test(request,pid):
     # post = Post.objects.get(id=pid)
-    post = get_object_or_404(Post,pk=pid)
+    post = get_object_or_404(Post,id=pid)
     content = {'post':post}
     return render(request, 'blog/test.html',content)
