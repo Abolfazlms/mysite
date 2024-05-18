4885a0a7864e5040e73f67a9e42fe878d52c81a8
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-05-18 21:48:16 +0330

    category added to this project.

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+class Category(models.Model):
+    name =   models.CharField(max_length=255)
+    def __str__(self):
+        return self.name 
+
 class Post(models.Model):
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)#models.ForeignKey(User,on_delete=models.CASCADE)
-    #image
+    image = models.ImageField(upload_to='blog/',default='blog/default.jpg')
     title = models.CharField(max_length = 255)
     content = models.TextField()
     #tags
-    #category
+    category = models.ManyToManyField(Category) # it's equal to category = models.ManyToManyField(Category,null=True)
     counted_views = models.IntegerField(default = 0)
     status = models.BooleanField(default = False)
     published_date = models.DateTimeField(null = True)
@@ -21,5 +26,4 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
 
