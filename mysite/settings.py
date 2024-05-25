74dd6bfdd64f32b5412344298a88222e5a3afe49
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-05-25 23:15:57 +0330

    static sitemap added to project.

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django_extensions',
     'website.apps.WebsiteConfig',
     'blog.apps.BlogConfig',
--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from website.views import *
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap
+from . import views
 
-app_name = 'website'
+app_name = 'website'\
+
+sitemaps = {'static':StaticViewSitemap}
 urlpatterns = [
     path('', index_view, name = 'index'),
     path('about/', about_view, name = 'about'),
@@ -9,4 +14,6 @@ urlpatterns = [
     path('elements/',elements_view, name = 'elements'),
     path('newsletter/',newsletter_view, name = 'newsletter'),
     path('test/',test_view, name = 'test'),
+    path('sitemap.xml',sitemap,{'sitemaps':sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
 ]
