eb3106b59fcda980ad94b5ac1fe80d204137ade0
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-05-25 14:01:05 +0330

    sites app added to project.

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django.contrib.sites',
     'django_extensions',
     'website.apps.WebsiteConfig',
     'blog.apps.BlogConfig',
 ]
+SITE_ID = 2
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
