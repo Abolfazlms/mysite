cafdc489596328658295ed37de2ddb1592899889
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-05-26 17:05:43 +0330

    change restored

--- website/urls.py
+++ website/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
 from website.views import *
-from django.contrib.sitemaps.views import sitemap
-from .sitemaps import StaticViewSitemap
-from . import views
+app_name = 'website'
 
-app_name = 'website'\
-
-sitemaps = {'static':StaticViewSitemap}
 urlpatterns = [
     path('', index_view, name = 'index'),
     path('about/', about_view, name = 'about'),
@@ -14,6 +9,4 @@ urlpatterns = [
     path('elements/',elements_view, name = 'elements'),
     path('newsletter/',newsletter_view, name = 'newsletter'),
     path('test/',test_view, name = 'test'),
-    path('sitemap.xml',sitemap,{'sitemaps':sitemaps},
-         name='django.contrib.sitemaps.views.sitemap'),
 ]
