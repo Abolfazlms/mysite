e73b0ee49cd7b405e9e2dec6f94b94b8f138a52d
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-06-02 17:11:47 +0330

    login section added to project.

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 def login_view(request):
-    return render(request,'accounts/login.html')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            # username = request.POST['username']
+            # password = request.POST['password']
+            form = AuthenticationForm(request,data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+
+                user = authenticate(request, username=username, password=password)
+
+                if user is not None:
+                    login(request, user)
+                    return redirect('/')
+                
+        form = AuthenticationForm()
+        context = {'form':form}
+        return render(request,'accounts/login.html',context)
+    else:
+        return redirect('/')
+
 def logout_view(request):
     pass
 def signup_view(request):
