e126dfdb0d738d56f00aac3bd2ba467d3ac6cbf8
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-06-03 21:05:45 +0330

    login with email method added to project.

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
-from .forms import UserCreationForm
+from .forms import UserCreationForm, EmailOrUsernameModelBackend
 
 # Create your views here.
 def login_view(request):
@@ -12,20 +12,33 @@ def login_view(request):
         if request.method == 'POST':
             # username = request.POST['username']
             # password = request.POST['password']
-            form = AuthenticationForm(request,data=request.POST)
-            if form.is_valid():
-                username = form.cleaned_data.get('username')
-                password = form.cleaned_data.get('password')
+            # form = AuthenticationForm(request,data=request.POST)
+            # if form.is_valid():
+            #     username = form.cleaned_data.get('username')
+            #     password = form.cleaned_data.get('password')
 
-                user = authenticate(request, username=username, password=password)
+            #     user = authenticate(request, username=username, password=password)
 
+            #     if user is not None:
+            #         login(request, user)
+            #         return redirect('/')
+            
+            username = request.POST['username']
+            password = request.POST['password']
+
+            user = EmailOrUsernameModelBackend.authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('/')
+            else:
+                user = EmailOrUsernameModelBackend.authenticate(request, email= username, password = password)
                 if user is not None:
-                    login(request, user)
+                    login(request,user)
                     return redirect('/')
                 
-        form = AuthenticationForm()
-        context = {'form':form}
-        return render(request,'accounts/login.html',context)
+        # form = AuthenticationForm()
+        # context = {'form':form}
+        return render(request,'accounts/login.html')
     else:
         return redirect('/')
 
@@ -41,7 +54,7 @@ def signup_view(request):
     if not request.user.is_authenticated:
         if request.method == 'POST':
             form = UserCreationForm(request.POST)
-            if form.is_valid:
+            if form.is_valid():
                 form.save()
                 # return redirect('accounts/login/')
                 return redirect('/')
