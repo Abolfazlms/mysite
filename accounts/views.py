19f0b7dd68ff91d37698a525790423bf38302e0b
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-06-03 12:55:59 +0330

    signup with email added to project.

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+
+from .forms import UserCreationForm
 
 # Create your views here.
 def login_view(request):
